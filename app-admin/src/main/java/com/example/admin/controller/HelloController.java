172d0dd16588b69366cb63d2d2bbe6eb985db2ac
chienaeae <chienaeae@gmail.com>
2024-09-26 16:45:30 -0400

    fix missing @RequestMapping

--- app-admin/src/main/java/com/example/admin/controller/HelloController.java
+++ app-admin/src/main/java/com/example/admin/controller/HelloController.java
@@ -4,9 +4,11 @@ import com.example.sharedlibrary.service.CommonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping
 public class HelloController {
 
     private final CommonService commonService;
--- app-client/src/main/java/com/example/client/controller/HelloController.java
+++ app-client/src/main/java/com/example/client/controller/HelloController.java
@@ -3,9 +3,11 @@ package com.example.client.controller;
 import com.example.sharedlibrary.service.CommonService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping
 public class HelloController {
 
     private final CommonService commonService;
